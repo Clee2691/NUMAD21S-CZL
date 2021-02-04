210c5f15b80469f2296af13bf4fda0fbda6aaaac
clee2691 <clee2691@gmail.com>
2021-02-04 15:05:46 -0500

    Add click activity layout, add string res

--- app/src/main/java/com/example/numad21s_czl/activity_click_letters.java
+++ app/src/main/java/com/example/numad21s_czl/activity_click_letters.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
 public class activity_click_letters extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_click_letters);
+    }
+
+    public void displayLetterClicked(){
+
     }
 }
