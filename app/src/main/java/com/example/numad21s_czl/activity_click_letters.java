dc777c7cb7d320d61bd2f016cc3ce524b29e9eea
clee2691 <clee2691@gmail.com>
2021-02-04 20:23:56 -0500

    Added function displaying button press, main layout tweak

--- app/src/main/java/com/example/numad21s_czl/activity_click_letters.java
+++ app/src/main/java/com/example/numad21s_czl/activity_click_letters.java
@@ -3,6 +3,8 @@ package com.example.numad21s_czl;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class activity_click_letters extends AppCompatActivity {
@@ -13,7 +15,30 @@ public class activity_click_letters extends AppCompatActivity {
         setContentView(R.layout.activity_click_letters);
     }
 
-    public void displayLetterClicked(){
+    public void displayLetterClicked(View view){
+        TextView displayBtnPress = findViewById(R.id.btn_pressed_textview);
+        String letter = "";
+        switch(view.getId()) {
+            case R.id.btn_a:
+                letter = getString(R.string.a_string);
+                break;
+            case R.id.btn_b:
+                letter = getString(R.string.b_string);
+                break;
+            case R.id.btn_c:
+                letter = getString(R.string.c_string);
+                break;
+            case R.id.btn_d:
+                letter = getString(R.string.d_string);
+                break;
+            case R.id.btn_e:
+                letter = getString(R.string.e_string);
+                break;
+            case R.id.btn_f:
+                letter = getString(R.string.f_string);
+                break;
+        }
+        displayBtnPress.setText(String.format("Pressed: %s", letter));
 
     }
 }
