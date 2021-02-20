f6c65ee3e77906c069337d29649e96a8e1071f6c
clee2691 <clee2691@gmail.com>
2021-02-20 18:09:39 -0500

    Added location permissions

--- app/src/main/java/com/example/numad21s_czl/ActivityLocator.java
+++ app/src/main/java/com/example/numad21s_czl/ActivityLocator.java
@@ -1,14 +1,53 @@
 package com.example.numad21s_czl;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
 
 import android.os.Bundle;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.util.Log;
 
 public class ActivityLocator extends AppCompatActivity {
+    private static String TAG = "LocatorActivity";
+    private static final int LOCATION_REQUEST_CODE = 50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locator);
+
+        setupPermissions();
+    }
+
+    private void setupPermissions() {
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission to access location data denied");
+            makeRequest();
+        }
+    }
+
+    protected void makeRequest() {
+        ActivityCompat.requestPermissions(this,
+                new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int reqCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (reqCode == LOCATION_REQUEST_CODE) {
+            if (grantResults.length == 0
+                    || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Log.i(TAG, "Permission Denied.");
+            } else {
+                Log.i(TAG, "Permission was granted!");
+            }
+        }
+
     }
 }
