029ed3d3431c1ac69d4e68f83aae70459275f3e7
clee2691 <clee2691@gmail.com>
2021-02-21 18:57:32 -0500

    Add button functionality to display location

--- app/src/main/java/com/example/numad21s_czl/ActivityLocator.java
+++ app/src/main/java/com/example/numad21s_czl/ActivityLocator.java
@@ -6,33 +6,67 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.core.app.ActivityCompat;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 
 public class ActivityLocator extends AppCompatActivity {
     private static String TAG = "LocatorActivity";
     private static final int LOCATION_REQUEST_CODE = 50;
 
+    private FusedLocationProviderClient fusedLocProv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locator);
-
-        setupPermissions();
+        fusedLocProv = LocationServices.getFusedLocationProviderClient(this);
+        checkLocationPermission();
     }
 
-    private void setupPermissions() {
+    public void onClickLocateMe(View view) {
+        TextView longTextView = findViewById(R.id.textView_longitude);
+        TextView latTextView = findViewById(R.id.textView_latitude);
+
+        OnSuccessListener<Location> locSuccess = new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    longTextView.setText(String.format("%.4f",location.getLongitude()));
+                    latTextView.setText(String.format("%.4f",location.getLatitude()));
+                }
+            }
+        };
+
+        if (checkLocationPermission()) { // If we have permission get location data
+            fusedLocProv.getLastLocation().addOnSuccessListener(this, locSuccess);
+        } else { // Request the location permission again.
+            requestLocationPermission();
+        };
+    }
+
+    private boolean checkLocationPermission() {
+        Button btn_displayLocation = findViewById(R.id.btn_displayLocation);
         int permission = ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION);
         if (permission != PackageManager.PERMISSION_GRANTED) {
-            Log.i(TAG, "Permission to access location data denied");
-            makeRequest();
+            requestLocationPermission();
+        } else {
+            return true;
         }
+        return false;
     }
 
-    protected void makeRequest() {
+    protected void requestLocationPermission() {
         ActivityCompat.requestPermissions(this,
                 new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
     }
@@ -40,14 +74,15 @@ public class ActivityLocator extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int reqCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
+        Button btn_displayLocation = findViewById(R.id.btn_displayLocation);
         if (reqCode == LOCATION_REQUEST_CODE) {
+            // Permission denied
             if (grantResults.length == 0
                     || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                Log.i(TAG, "Permission Denied.");
-            } else {
-                Log.i(TAG, "Permission was granted!");
+                btn_displayLocation.setClickable(false);
+            } else { // Permission accepted
+                btn_displayLocation.setClickable(true);
             }
         }
-
     }
 }
